25ad4898afa12daa5b1217007c1dd06122500dbf
Kaapija <kaapija@gmail.com>
2016-02-03 22:51:24 +0200

    App for shooting in Sr5

---
+++ src/main/Sr5Helper.java
@@ -0,0 +1,25 @@
+package main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author kaapi
+ */
+public class Sr5Helper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainMenu form = new mainMenu();
+       // form.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("sr_kuva.png")));
+        form.setVisible(true);
+    }
+    
+}
